7970d2b65c094caf2f3ae17b34f7f9c724135f32
Alexey Grachov <grachov.alexey@gmail.com>
2015-10-16 11:31:25 +0300

    Go fmt.

--- automata.go
+++ automata.go
@@ -1,147 +1,145 @@
 package automata
 
 import (
-  "bufio"
-  "errors"
-  "github.com/op/go-logging"
-  "github.com/tarm/goserial"
-  "io"
-  "net"
-  "os"
-  "time"
+	"bufio"
+	"errors"
+	"io"
+	"net"
+	"os"
+	"time"
 )
 
 const (
-  SerialArduino   = 0
-  EthernetArduino = 1
+	SerialArduino   = 0
+	EthernetArduino = 1
 
-  Ping             = 0
-  SetDigitalOutput = 1
-  DigitalWriteHigh = 2
-  DigitalWriteLow  = 3
-  GetTemp          = 4
+	Ping             = 0
+	SetDigitalOutput = 1
+	DigitalWriteHigh = 2
+	DigitalWriteLow  = 3
+	GetTemp          = 4
 )
 
 type Message struct {
-  Command   byte
-  Parameter byte
+	Command   byte
+	Parameter byte
 }
 
 type Arduino struct {
-  conn      io.ReadWriteCloser
-  pins      map[byte]bool
-  messages  chan Message
-  responses chan []byte
+	conn      io.ReadWriteCloser
+	pins      map[byte]bool
+	messages  chan Message
+	responses chan []byte
 }
 
 var log = logging.MustGetLogger("example")
 
 func initLogger() {
-  backend1 := logging.NewLogBackend(os.Stderr, "", 0)
-  backend1Leveled := logging.AddModuleLevel(backend1)
-  backend1Leveled.SetLevel(logging.ERROR, "")
-  logging.SetBackend(backend1Leveled)
+	backend1 := logging.NewLogBackend(os.Stderr, "", 0)
+	backend1Leveled := logging.AddModuleLevel(backend1)
+	backend1Leveled.SetLevel(logging.ERROR, "")
+	logging.SetBackend(backend1Leveled)
 }
 
 func New(t byte, addr string) (*Arduino, error) {
-  var err error
-  var conn io.ReadWriteCloser
-  initLogger()
-  switch t {
-  case SerialArduino:
-    c := &serial.Config{Name: addr, Baud: 57600}
-    conn, err = serial.OpenPort(c)
-  case EthernetArduino:
-    conn, err = net.DialTimeout("tcp", addr, 5 * time.Second)
-  default:
-    return nil, errors.New("Undefined arduino connection type.")
-  }
-  if err != nil {
-    log.Debug("Error connecting to arduino on %s, error: %v", addr, err)
-    return nil, err
-  }
-  ar := newArduino(conn)
-  return ar, nil
+	var err error
+	var conn io.ReadWriteCloser
+	initLogger()
+	switch t {
+	case SerialArduino:
+		c := &serial.Config{Name: addr, Baud: 57600}
+		conn, err = serial.OpenPort(c)
+	case EthernetArduino:
+		conn, err = net.DialTimeout("tcp", addr, 5*time.Second)
+	default:
+		return nil, errors.New("Undefined arduino connection type.")
+	}
+	if err != nil {
+		log.Debug("Error connecting to arduino on %s, error: %v", addr, err)
+		return nil, err
+	}
+	ar := newArduino(conn)
+	return ar, nil
 }
 
 func newArduino(conn io.ReadWriteCloser) *Arduino {
-  log.Debug("Instantiate arduino on connection %v", conn)
-  ar := new(Arduino)
-  log.Debug("Arduino isn't ready yet.")
-  ar.conn = conn
-  ar.pins = make(map[byte]bool)
-  log.Debug("Launching messageHandler in goroutine")
-  go ar.messageHandler()
-  log.Debug("Pinging...")
-  ar.Ping()
-  return ar
+	log.Debug("Instantiate arduino on connection %v", conn)
+	ar := new(Arduino)
+	log.Debug("Arduino isn't ready yet.")
+	ar.conn = conn
+	ar.pins = make(map[byte]bool)
+	log.Debug("Launching messageHandler in goroutine")
+	go ar.messageHandler()
+	log.Debug("Pinging...")
+	ar.Ping()
+	return ar
 }
 
 func (ar *Arduino) Close() {
-  ar.conn.Close()
+	ar.conn.Close()
 }
 
 func (ar *Arduino) sendCommand(command byte, parameter byte) []byte {
-  log.Debug("sending %v, %v", command, parameter)
-  ar.messages <- Message{Command: command, Parameter: parameter}
-  response := <-ar.responses
-  return response
+	log.Debug("sending %v, %v", command, parameter)
+	ar.messages <- Message{Command: command, Parameter: parameter}
+	response := <-ar.responses
+	return response
 }
 
 func (ar *Arduino) Ping() {
-  log.Debug("Sleeping for 2 seconds...")
-  time.Sleep(2 * time.Second)
-  ar.sendCommand(Ping, 0)
+	log.Debug("Sleeping for 2 seconds...")
+	time.Sleep(2 * time.Second)
+	ar.sendCommand(Ping, 0)
 }
 
 func (ar *Arduino) SetDigitalOutput(pin byte) {
-  ar.sendCommand(SetDigitalOutput, pin)
+	ar.sendCommand(SetDigitalOutput, pin)
 }
 
 func (ar *Arduino) On(pin byte) byte {
-  ar.pins[pin] = true
-  return ar.sendCommand(DigitalWriteHigh, pin)[3]
+	ar.pins[pin] = true
+	return ar.sendCommand(DigitalWriteHigh, pin)[3]
 }
 
 func (ar *Arduino) Off(pin byte) byte {
-  ar.pins[pin] = false
-  return ar.sendCommand(DigitalWriteLow, pin)[3]
+	ar.pins[pin] = false
+	return ar.sendCommand(DigitalWriteLow, pin)[3]
 }
 
 func (ar *Arduino) Toggle(pin byte) byte {
-  if ar.pins[pin] == true {
-    return ar.Off(pin)
-  }
-  return ar.On(pin)
+	if ar.pins[pin] == true {
+		return ar.Off(pin)
+	}
+	return ar.On(pin)
 }
 
 func (ar *Arduino) Temp() []byte {
-  return ar.sendCommand(GetTemp, 0)
+	return ar.sendCommand(GetTemp, 0)
 }
 
 func (ar *Arduino) messageHandler() {
-  log.Debug("Starting messageHandler")
-  ar.messages = make(chan Message, 256)
-  ar.responses = make(chan []byte)
-  log.Debug("Channels initialized")
+	log.Debug("Starting messageHandler")
+	ar.messages = make(chan Message, 256)
+	ar.responses = make(chan []byte)
+	log.Debug("Channels initialized")
 
-  for {
-    log.Debug("Trying to get message from channel")
-    message := <-ar.messages
-    log.Debug("Message received from channel: %v", message)
-    buf := make([]byte, 4)
-    log.Debug("Writing message to connection %v", []byte{message.Command, message.Parameter})
+	for {
+		log.Debug("Trying to get message from channel")
+		message := <-ar.messages
+		log.Debug("Message received from channel: %v", message)
+		buf := make([]byte, 4)
+		log.Debug("Writing message to connection %v", []byte{message.Command, message.Parameter})
 
-    ar.conn.Write([]byte{message.Command, message.Parameter})
-    log.Debug("Reading message from connection")
-    reader := bufio.NewReader(ar.conn)
+		ar.conn.Write([]byte{message.Command, message.Parameter})
+		log.Debug("Reading message from connection")
+		reader := bufio.NewReader(ar.conn)
 
-    buf[0], _ = reader.ReadByte()
-    buf[1], _ = reader.ReadByte()
-    buf[2], _ = reader.ReadByte()
-    buf[3], _ = reader.ReadByte()
+		buf[0], _ = reader.ReadByte()
+		buf[1], _ = reader.ReadByte()
+		buf[2], _ = reader.ReadByte()
+		buf[3], _ = reader.ReadByte()
 
-    log.Debug("Response from connection received, adding to channel %v", buf)
-    ar.responses <- buf
-  }
+		log.Debug("Response from connection received, adding to channel %v", buf)
+		ar.responses <- buf
+	}
 }
--- automata_test.go
+++ automata_test.go
@@ -1,67 +1,67 @@
 package automata
 
 import (
-  "testing"
-  "time"
+	"testing"
+	"time"
 )
 
 func TestNewSerial(t *testing.T) {
-  ar, err := NewSerial("/dev/invalid")
-  if ar != nil || err == nil {
-    t.Error("Should not create arduino for invalid port")
-  }
+	ar, err := NewSerial("/dev/invalid")
+	if ar != nil || err == nil {
+		t.Error("Should not create arduino for invalid port")
+	}
 }
 
 func TestNewEthernet(t *testing.T) {
-  _, err := NewEthernet("169.254.68.110:13666")
-  if err != nil {
-    t.Error("Should create arduino via ethernet")
-  }
+	_, err := NewEthernet("169.254.68.110:13666")
+	if err != nil {
+		t.Error("Should create arduino via ethernet")
+	}
 }
 
 func TestSendCommand(t *testing.T) {
-  // ar, _ := NewSerial("/dev/tty.usbmodem1421")
-  ar, _ := NewEthernet("169.254.68.110:13666")
-  ar.SetDigitalOutput(12)
-  for i := 0; i < 10; i++ {
-    go func() {
-      resp := ar.On(12)
-      if resp != DigitalWriteHigh {
-        t.Error("On should respond with DigitalWriteHigh")
-      }
-    }()
+	// ar, _ := NewSerial("/dev/tty.usbmodem1421")
+	ar, _ := NewEthernet("169.254.68.110:13666")
+	ar.SetDigitalOutput(12)
+	for i := 0; i < 10; i++ {
+		go func() {
+			resp := ar.On(12)
+			if resp != DigitalWriteHigh {
+				t.Error("On should respond with DigitalWriteHigh")
+			}
+		}()
 
-    go func() {
-      temp := ar.Temp()
-      t.Log(temp)
-      if temp[2] == 0 && temp[3] == 0 {
-        t.Errorf("Temp should not be 0, got %v", temp)
-      }
-    }()
+		go func() {
+			temp := ar.Temp()
+			t.Log(temp)
+			if temp[2] == 0 && temp[3] == 0 {
+				t.Errorf("Temp should not be 0, got %v", temp)
+			}
+		}()
 
-    go func() {
-      resp := ar.Off(12)
-      if resp != DigitalWriteLow {
-        t.Error("Off should respond with DigitalWriteHigh")
-      }
-    }()
+		go func() {
+			resp := ar.Off(12)
+			if resp != DigitalWriteLow {
+				t.Error("Off should respond with DigitalWriteHigh")
+			}
+		}()
 
-    go func() {
-      ar.Toggle(12)
-    }()
-  }
-  time.Sleep(4 * time.Second)
+		go func() {
+			ar.Toggle(12)
+		}()
+	}
+	time.Sleep(4 * time.Second)
 }
 
 func TestToggle(t *testing.T) {
-  ar, _ := NewSerial("/dev/tty.usbmodem1411")
-  ar.SetDigitalOutput(12)
-  resp := ar.Toggle(12)
-  if resp != DigitalWriteHigh {
-    t.Error("Toggle should write digital high first")
-  }
-  resp = ar.Toggle(12)
-  if resp != DigitalWriteLow {
-    t.Error("Toggle should write digital low second")
-  }
+	ar, _ := NewSerial("/dev/tty.usbmodem1411")
+	ar.SetDigitalOutput(12)
+	resp := ar.Toggle(12)
+	if resp != DigitalWriteHigh {
+		t.Error("Toggle should write digital high first")
+	}
+	resp = ar.Toggle(12)
+	if resp != DigitalWriteLow {
+		t.Error("Toggle should write digital low second")
+	}
 }
